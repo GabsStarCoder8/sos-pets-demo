f1af9b978b32ce2666e3f23550dc99730d9d9a42
GabsStarCoder8 <gliberato2018@gmail.com>
2026-06-18 17:35:22 -0400

    feat: implementa tela de login e adiciona arquivos do projeto

---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //definição d cor azul principal
+    const Color primaryColor = Color(0xFF2D6A9F);
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //imagem do dog
+                Image.asset('assets/images/cachorro.png', height: 150),
+                const SizedBox(height: 10),
+                //nome do app
+                const Text(
+                  'S.O.S Pets',
+                  style: TextStyle(
+                    fontSize: 62,
+                    fontWeight: FontWeight.w900,
+                    color: primaryColor,
+                  ),
+                ),
+                const SizedBox(height: 30),
+
+                //title bem-vindo
+                const Text(
+                  'Bem-vindo!',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: primaryColor,
+                  ),
+                ),
+                const SizedBox(height: 30),
+
+                //campo e-mail
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    hintText: 'Digite seu e-mail',
+                    hintStyle: const TextStyle(color: Color(0xFF5A809E)),
+
+                    //borda padrão
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: Colors.black12),
+                    ),
+
+                    //borda quando clicar no campo
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: primaryColor),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+
+                //campo de sennha
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Senha',
+                    hintStyle: const TextStyle(color: Color(0xFF5A809E)),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: Colors.black12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: primaryColor),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+
+                //esqueceu a senha
+                Align(
+                  alignment: AlignmentGeometry.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Esqueceu a senha?',
+                      style: TextStyle(
+                        color: Color(0xFF87B4E0),
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 24),
+
+                //botão entrar
+                SizedBox(
+                  width: double.infinity,
+                  height: 55,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: primaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Entrar',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 85),
+
+                //rodapé (nov aqui? cadastrar-se)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Novo aqui?',
+                      style: TextStyle(
+                        color: primaryColor,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: primaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 12,
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Cadastrar-se',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
